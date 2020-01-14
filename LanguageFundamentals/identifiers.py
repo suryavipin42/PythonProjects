1ce0e6625850820746cecc3d6626ea28edf70a63
suryavipin42 <suryavipin42@gmail.com>
2020-01-14 10:56:58 +0530

    secondcommit

---
+++ LanguageFundamentals/identifiers.py
@@ -0,0 +1,11 @@
+# variable name= value
+# variable represents the memory location
+#start with character, no length limit,but use max upto 4
+# identifier = name of class,function ,variable,method
+
+# no= 2
+# print(no,"is the value in no.")
+brand = "Apple"
+exchangeRate = 1.235235245
+message = "The price of this %s laptop is" % brand
+print (message)
---
+++ LanguageFundamentals/swap.py
@@ -0,0 +1,14 @@
+# input
+print ("Enter the value of a")
+a = int(input())
+print ("Enter the value of b")
+b = int(input())
+temp = a
+a = b
+b = temp
+# a=a+b
+# b=a-b
+# a=a-b
+
+# output
+print("After Swap : a = {} b = {}".format(a,b))
